e644a78cfa5bf2a7f8ff3d512bb3ad0cb9729518
Sebastián Restrepo <sebastianrestrepoq@gmail.com>
2018-04-16 08:41:21 -0500

    html + style

--- index.js
+++ index.js
@@ -1,53 +1,26 @@
-const express = require('express');
-const app = express();
-const fs = require('fs');
+const express = require('express'),
+    fs = require('fs'),
+    consolidate = require('consolidate'),
+    hbs = require('handlebars'),
+    app = express();
 
-app.get('/', (req, res) => res.send('Working!'));
+//Configure the render engine (express + consolidate -> handlebars)
+app.engine('hbs', consolidate.handlebars);
+app.set('view engine', 'hbs');
+app.set('views', './views');
+
+app.use('/css', express.static('css'));
 
 app.listen(6060, () => console.log('Example app listening on port 6060!'));
 
-
-var text;
 var name, phone, genre;
 
-//Read the whole folder
-var folder = fs.readdirSync('people');
+//Read the whole directory folder
+var folders = fs.readdirSync('people');
 
 //Write the HTML with the folder's name
-app.get('/dir', (req, res) => {
-    for (var i = 0; i < folder.length; i++) {
-        res.send(getHtml(folder));
-    }
+app.get('/', (req, res) => {
+    res.render('index', {
+        folders: folders
+    });
 });
-
-//Read the file
-
-//console.log('This is the file: ' + file);
-
-fs.writeFileSync('testing2.txt', 'Hola? Como estoy');
-
-var getHtml = function (name, phone, genre) {
-    return `
-    <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=<<device-width>>, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>People</title>
-    <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,400i,700,700i,800" rel="stylesheet"> 
-</head>
-<body>
-    <h1>People</h1>
-    <div id="people-list">
-    <a href="localhost:6060">${name}</a> 
-    </div>
-    <label for="">Name</label>
-    <input type="text" id="name" name="name">
-    <button id="show">Add</button>
-    <script src="index.js"></script> 
-</body>
-</html>
-    
-    `;
-}
